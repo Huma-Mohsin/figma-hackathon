5ab8f982621f99f126c99008094fe11d6ae03828
Huma-Mohsin <160951214+Huma-Mohsin@users.noreply.github.com>
2024-12-09 01:04:08 +0500

    task completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
-import Product from "./[Products]/page";
+
 import BestAirMax from "./components/BestAirmax4";
-import Cart from "./components/Cart";
+
 import DoNotMissSection from "./components/DonotMiss";
 import Essentials from "./components/Essentials";
 import Footer from "./components/Footer";
 import GearUp from "./components/GearUp6";
 
 import HelloNikeText from "./components/HelloNikeText2";
-import LoginForm from "./components/Login";
-import Navbar from "./components/Navbar1";
+
 import NikeAirPlus from "./components/NikeAirPlus3";
-import StaticFooter from "./components/StaticFooter";
+
 import StepIntoShoes from "./components/StepInToShoes5";
 
 
